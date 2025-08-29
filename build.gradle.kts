82e4e313c7f138bebbf7939f5f1b7345e40d8c62
Awais Zaka <az@intsoftdev.com>
2025-08-29 21:13:23 +0100

    remove unneeded dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(isdlibs.plugins.androidApplication) apply false
-    alias(isdlibs.plugins.androidLibrary) apply false
     alias(isdlibs.plugins.composeMultiplatform) apply false
     alias(isdlibs.plugins.compose.compiler) apply false
     alias(isdlibs.plugins.kotlinMultiplatform) apply false
